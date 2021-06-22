d1a76bab5196d7affa1e6860b8854cd3055a399c
Alex <a.rodarte@alphasoluciones.com>
2021-06-22 18:35:47 -0500

    se implementan los action con safearguments

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -28,9 +28,11 @@ import com.example.android.navigation.databinding.FragmentGameOverBinding
 class GameOverFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+        binding.tryAgainButton.setOnClickListener { view:View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment2_to_gameFragment)
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -39,9 +39,13 @@ import android.content.pm.PackageManager
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener { view:View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context,"NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
